6e48b4262d7e64574e581e357ce924a0288da0d2
H. M. Masum Billah <mbillah21@gmail.com>
2023-11-11 23:19:49 +0600

    Live Test 07 Completed

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Route:
 Login Method: 
 ```php
     public function login($email, $password): string{
-        if ($email == 'masum@billah.com' && $password == '12345678') {
+        if ($email === 'masum@billah.com' && $password === '12345678') {
             return 'Login successful';
         }else{
             return 'Invalid credentials';
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function login($email, $password): string{
-        if ($email == 'masum@billah.com' && $password == '12345678') {
+        if ($email === 'masum@billah.com' && $password === '12345678') {
             return 'Login successful';
         }else{
             return 'Invalid credentials';
